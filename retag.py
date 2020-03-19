e83d7328d79ad7acde4caa49c97f2d6b670aab9a
Cloud User <centos@ip-172-31-77-93.ec2.internal>
2020-03-19 17:57:38 +0000

    id all tags not on origin master

--- retag.py
+++ retag.py
@@ -8,7 +8,7 @@ import sys
 
 def cli():
     parser = argparse.ArgumentParser(description='Check and re-write tags')
-    parser.add_argument('tag', nargs='?', help='Git tag')
+    parser.add_argument('tags', nargs='*', help='Git tag')
     parser.add_argument('--dry-run', action='store_true', help='Do not rewrite tag. Useful to check for errors.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print more.')
     return parser
@@ -36,31 +36,37 @@ def main(cli_args: list = None):
         print(f'-- Parsed arguments: {args}')
 
     # Determine the tag to POST
-    if args.tag is None:
-        # find the most recent tag
-        tag = subprocess.check_output('git describe --tags --abbrev=0', shell=True).decode().strip()
+    if not args.tags:
+        all_tags = set(subprocess.check_output("git tag", shell=True).decode().splitlines())
     else:
-        tag = args.tag
+        all_tags = args.tags
 
     if args.verbose:
-        print(f'-- The tag to inspect: {tag}')
+        print(f'-- All known tags: {all_tags}')
+    
+    remote_tags = set(subprocess.check_output("git tag --merged origin/master", shell=True).decode().splitlines())
+    
+    remaining_tags = all_tags - remote_tags
+    
+    if args.verbose:
+        print(f'-- Tags not on origin/master: {remaining_tags}')
 
-    if is_annotated(tag):
-        commit = subprocess.check_output(f'git rev-parse $(git rev-parse {tag})^{{}}', shell=True).decode().strip()
-        if args.verbose:
-            print(f'-- Annotated tag {tag} associated with commit {commit} to replace')
-
-        if not args.dry_run:
-            ret = subprocess.check_output(f'git tag -d {tag}', shell=True)
+    for tag in remaining_tags:           
+        if is_annotated(tag):
+            commit = subprocess.check_output(f'git rev-parse $(git rev-parse {tag})^{{}}', shell=True).decode().strip()
             if args.verbose:
-                print(f'-- Annotated tag {tag} deleted.')
+                print(f'-- Annotated tag {tag} associated with commit {commit} to replace')
 
-            ret = subprocess.check_output(f'git tag {tag} {commit}', shell=True)
-            if args.verbose:
-                print(f'-- Lightweight tag {tag} created.')
-        else:
-            sys.exit(0)
+            if not args.dry_run:
+                ret = subprocess.check_output(f'git tag -d {tag}', shell=True)
+                if args.verbose:
+                    print(f'-- Annotated tag {tag} deleted.')
+
+                ret = subprocess.check_output(f'git tag {tag} {commit}', shell=True)
+                if args.verbose:
+                    print(f'-- Lightweight tag {tag} created.')
+        
 
 if __name__ == '__main__':
     main()
-
+	
