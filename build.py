ca5c9984c88b1c0ade1cf1208faafbeaae732b86
Albert DeFusco <albert.defusco@me.com>
2020-05-05 14:25:40 +0000

    push image with argument

--- build.py
+++ build.py
@@ -13,17 +13,25 @@ def start_server(path, port=8000):
     httpd.serve_forever()
 
 
-def docker_build(base_editor, vscode='vscode'):
-    vscode_image = '{}-{}'.format(base_editor, vscode)
+def docker_build(base_editor, suffix='vscode', push=False):
+    vscode_image = '{}-{}'.format(base_editor, suffix)
     subprocess.check_call('docker build --network=host --build-arg WORKSPACE={base_editor} -t {vscode_image} .'
-                          .format(base_editor=base_editor, vscode_image=vscode_image))
+                            .format(base_editor=base_editor, vscode_image=vscode_image),
+                          shell=True)
+
+    if push:
+        subprocess.check_call('docker push {}'.format(vscode_image), shell=True)
 
 
 def cli():
     parser = argparse.ArgumentParser(description='Build VSCode docker image')
 
     parser.add_argument('--base-editor', help='Base editor image name. If not provided the value is computed from the current platform version.')
-    parser.add_argument('--airgapped', help='Run a local webserver to transfer temporary files into the image.')
+    parser.add_argument('--suffix', help='Suffix to apply to base editor image when buidling the VSCode image. Default: vscode',
+                        type=str, default='vscode')
+    parser.add_argument('--push', help='Push VSCode image if built successfully.', action='store_true')
+    parser.add_argument('--airgapped', help='Run a local webserver to transfer temporary files into the image.',
+                        action='store_true')
 
     return parser
 
@@ -32,20 +40,21 @@ def main(args):
     if args.airgapped:
         daemon = threading.Thread(name='daemon_server',
                                   target=start_server,
-                                  args=('downloads')
+                                  args=('downloads'))
         daemon.setDaemon(True) # Set as a daemon so it will be killed once the main thread is dead.
         daemon.start()
     
     if args.base_editor:
         base_editor = args.base_editor
     else:
-        platform_version = subprocess.check_output('kubectl exec -it `kubectl get pods -l app=ap-ui -o name` -- env | grep PLATFORM_VERSION | awk -NF= "{print $2}"',
-                                                   shell=True))
-        base_editor = 'leader.telekube.local:5000/ae-workspace:{}'.format(platform_version)
+        platform_version = subprocess.check_output('kubectl exec -it `kubectl get pods -l app=ap-ui -o name` -- env | grep PLATFORM_VERSION',
+                                                   shell=True)
+        base_editor = 'leader.telekube.local:5000/ae-editor:{}'.format(platform_version.decode().strip().split('=')[1])
     
-    docker_build(base_editor)
+    docker_build(base_editor, args.suffix, args.push)
 
 
 if __name__ == '__main__':
     args = cli().parse_args()
     main(args)
+
